20aa3c4e68f939eb582622faeb66469cba37e914
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-11-11 09:25:50 +0600

    read box

--- home/Box.py
+++ home/Box.py
@@ -2,8 +2,8 @@ import pygame
 
 pygame.init()
 
-color = (000,000,000)
-position = (0,0)
+color = (255,255,255)
+rect_color = (255,0,0)
 
 # CREATING CANVAS
 canvas = pygame.display.set_mode((500,500))
@@ -16,10 +16,10 @@ exit = False
 
 while not exit:
     canvas.fill(color)
-    canvas.blit(image, dest = position)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
 
+    pygame.draw.rect(canvas, rect_color,
+                     pygame.Rect(30,30,60,60))
     pygame.display.update()
